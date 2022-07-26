32d44e9866400cbb9917054538a99e14f1331809
Romuald Brillout <git@brillout.com>
2022-07-26 16:44:36 +0200

    wip

--- index.mjs
+++ index.mjs
@@ -2,13 +2,20 @@ import { init, parse } from 'es-module-lexer'
 await init
 
 parseAndLog("export * from 'bla'")
-console.log()
-parseAndLog("import * as nam from 'bla'")
+console.log('\n')
+parseAndLog("import * as pkg from 'bla'")
+console.log('\n')
+parseAndLog("import { mod } from 'bla'; export { mod }")
+console.log('\n')
+parseAndLog("export { mod } from 'bla'")
+console.log('\n')
 
 function parseAndLog(str) {
   console.log('str:', str)
   const parsed = parse(str)
   console.log('parsed:', parsed)
-  console.log('substring:', str.substring(parsed[0][0].s, parsed[0][0].e))
-  console.log('substring statement:', str.substring(parsed[0][0].ss, parsed[0][0].se))
+  const [imports, exports] = parsed
+  console.log('exports:', exports)
+  console.log('first import name:', str.substring(imports[0].s, imports[0].e))
+  console.log('first import statement:', str.substring(imports[0].ss, imports[0].se))
 }
